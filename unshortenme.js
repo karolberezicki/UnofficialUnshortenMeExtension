de751cecd498039252d0b2552eab90e7b44bfe95
Karol Berezicki <karolberezicki@gmail.com>
2017-10-01 12:10:30 +0200

    Refactor, change request to promise

--- unshortenme.js
+++ unshortenme.js
@@ -1,30 +1,33 @@
+function getUnshortenedUrl(shortenedUrl) {
+    return new Promise((resolve, reject) => {
+        fetch(`https://unshorten.me/json/${shortenedUrl}`)
+            .then(response => {
+                const result = response.json();
+                resolve(result);
+            })
+            .catch(err => reject(err));
+    })
+}
+
+function chromeOpenLinkInNextTab(url) {
+    chrome.tabs.query({ active: true }, tabs => {
+        let index = tabs[0].index;
+        chrome.tabs.create({
+            url: url,
+            index: index + 1
+        });
+    });
+}
+
+
 function onClickHandler(info, tab) {
-    console.log("item " + info.menuItemId + " was clicked");
-    console.log("selection " + info.selectionText);
-    console.log("info: " + JSON.stringify(info));
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://unshorten.me/json/" + info.selectionText, true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
-            console.log(xhr.responseText);
-            var resp = JSON.parse(xhr.responseText);
-            console.log(resp);
-
-            if (resp.success) {
-                chrome.tabs.query({
-                    active: true
-                }, tabs => {
-                    let index = tabs[0].index;
-                    chrome.tabs.create({
-                        url: resp.resolved_url,
-                        index: index + 1
-                    });
-                });
-            }
-        }
-    }
-    xhr.send();
+    getUnshortenedUrl(info.selectionText)
+    .then(resp =>
+    {
+        console.log(resp);
+        if (resp.success) chromeOpenLinkInNextTab(resp.resolved_url);
+    })
+    .catch(err => console.log(err));
 };
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
